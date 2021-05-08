f95db4a758446bf48beaa26679b4dc8ea19d536a
Nathan Chow <emailme@nathanchow.ca>
2021-05-08 16:31:09 -0400

    update friends

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
@@ -15,18 +22,29 @@ const client = new ApolloClient({
 })
 
 function App() {
-  return ( 
-    //wrap ApolloProvider around the JSX code to use GraphQL with Apollo
+  return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
+
+// <Route component={NoMatch} within <Switch> means that if there is no path, it will direct to NoMatch, a 404 page
 
 export default App;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 
 // wrap entire query code in tagged template literal using imported gql function, now we can import the query function by name and use it anywhere in the front end application
+// Displays all THOUGHT(S)
 export const QUERY_THOUGHTS = gql`
     query thoughts($username: String) {
         thoughts(username: $username) {
@@ -17,4 +18,45 @@ export const QUERY_THOUGHTS = gql`
             }
         }
     }
+`;
+
+// Displays single THOUGHT
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+// displays USER
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
 `;
