8c24d98ae302ee7392da133633b2929f4b3d699a
Nathan Chow <emailme@nathanchow.ca>
2021-05-11 17:31:53 -0400

    update home/ thought form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,13 +2,17 @@ import React from 'react';
 //Redirect allows to redirect the user to another route within the application
 import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
-import { useQuery } from '@apollo/react-hooks';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Profile = () => {
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   //gets the username from address bar localhost:3000/profile/:username?
   const { username: userParam } = useParams();
@@ -40,12 +44,29 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+
+        {/* // will not display the ADD FRIEND button if link is /profile */}
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -61,6 +82,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
